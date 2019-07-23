51309d6877a7210e3739a833143a19ec90d89808
Chris VanderKooi <chrisvanderkooi@gmail.com>
2019-07-23 11:20:23 -0600

    Added new mac setup script

---
+++ setup.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+set -euo pipefail
+
+# Display message 'Setting up your Mac...'
+echo "Setting up Mac..."
+sudo -v
+
+# Homebrew - Installation
+echo "Installing Homebrew"
+
+if test ! $(which brew); then
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+
+# Install Homebrew Packages
+
+cd ~
+echo "Installing Homebrew packages"
+
+homebrew_packages=(
+  "git"
+  "mysql"
+  "php"
+  "sqlite"
+  "node"
+)
+
+for homebrew_package in "${homebrew_packages[@]}"; do
+  brew install "$homebrew_package"
+done
+
+# Install Casks
+echo "Installing Homebrew cask packages"
+brew tap caskroom/fonts
+
+homebrew_cask_packages=(
+  "authy"
+  "discord"
+  "font-fira-code"
+  "font-quicksand"
+  "google-chrome"
+  "postman"
+  "iterm2"
+  "rocket"
+  "spotify"
+  "sublime-text"
+  "sketch"
+  "visual-studio-code"
+)
+
+for homebrew_cask_package in "${homebrew_cask_packages[@]}"; do
+  brew cask install "$homebrew_cask_package"
+done
+
+# Install Composer
+echo "Installing Composer"
+curl -sS https://getcomposer.org/installer | php
+mv composer.phar /usr/local/bin/composer
+
+# Install Global Composer Packages
+echo "Installing Global Composer Packages"
+/usr/local/bin/composer global require laravel/installer laravel/valet statamic/cli
+
+# Install Laravel Valet
+echo "Installing Laravel Valet"
+$HOME/.composer/vendor/bin/valet install
+
+# Create Sites directory
+echo "Creating a Sites directory"
+mkdir $HOME/Sites
+
+# Start MySQL for the first time
+echo "Starting MySQL for the first time"
+brew services start mysql
+
+# Configure Laravel Valet
+cd ~/Sites
+valet park && cd ~
+echo "Configuring Laravel Valet"
+cd ~
+valet restart
+
+# Installing Global Node Dependecies
+echo "Installing Global Node Dependecies"
+npm install -g @angular/cli
+npm install -g firebase-tools
+
+# Generate SSH key
+echo "Generating SSH keys"
+ssh-keygen -t rsa
+
+echo "Copied SSH key to clipboard - You can now add it to Github"
+pbcopy < ~/.ssh/id_rsa.pub
+
+# Complete
+echo "Installation Complete"
